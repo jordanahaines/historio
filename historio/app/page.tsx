99c9043a320635fdbb8ddf2ee96769cab417ef9e
Jordan <jordanhaines@runforsomething.net>
2024-09-20 16:39:54 -0500

    Got sass installed and working

--- historio/app/page.tsx
+++ historio/app/page.tsx
@@ -1,16 +1,20 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+import { Link } from "@nextui-org/link"
+import { Snippet } from "@nextui-org/snippet"
+import { Code } from "@nextui-org/code"
+import { button as buttonStyles } from "@nextui-org/theme"
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import styles from "./page.module.scss"
+
+import { siteConfig } from "@/config/site"
+import { title, subtitle } from "@/components/primitives"
+import { GithubIcon } from "@/components/icons"
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
+      <div
+        className={`inline-block max-w-xl text-center justify-center ${styles.pageRed}`}
+      >
         <span className={title()}>Make&nbsp;</span>
         <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
         <br />
@@ -52,5 +56,5 @@ export default function Home() {
         </Snippet>
       </div>
     </section>
-  );
+  )
 }

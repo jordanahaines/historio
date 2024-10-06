7f2ab5bab5333de75575f08a7934f55e3d6c6408
Jordan <jordanhaines@runforsomething.net>
2024-10-05 19:52:16 -0500

    Updating models for researchers and insights

---
+++ historio/db/schema/research.ts
@@ -0,0 +1,16 @@
+import { boolean, numeric, pgTable, uuid, varchar } from "drizzle-orm/pg-core"
+
+import { books } from "./book"
+import { BASE_SCHEMA_FIELDS } from "./common"
+
+export const researcherRuns = pgTable("researcher_runs", {
+  ...BASE_SCHEMA_FIELDS,
+  researcher_key: varchar("researcher_key"),
+  book_id: uuid("book_id").references(() => books.id),
+  failure: boolean("failure").default(false),
+  error: varchar("error"),
+  new_insights: numeric("new_insights"),
+})
+
+export type SelectResearcherRun = typeof researcherRuns.$inferSelect
+export type InsertResearcherRun = typeof researcherRuns.$inferInsert
